1ccccf88a5226e16604c86de4f0ebe026f60f803
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-06-23 23:04:46 +0300

    Read File Content

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const init = () => {
-
+    document.querySelector('#filename').value = '';
 }
-function uploadFile(){
+function getFileBytes(event) {
+    event.preventDefault();
+    let photo = document.getElementById("filename").files[0];
+    const result = event.target.filename;
+    const reader = new FileReader();
+    const file =  reader.readAsBinaryString(photo)
+    let fileContent = ""
+    reader.addEventListener('load', (e) => {
+        fileContent = e.target.result;
+    })
+    return fileContent;
+}
+function uploadFile(fileBytes){
     
 }
-
+const form = document.querySelector("form")
+form.addEventListener("submit", getFileBytes)
 document.addEventListener("DOMContentLoaded", init)
